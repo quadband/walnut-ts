e000a86cf0c1f25552090c7b58389e4e8481697f
Dave <david@quadband.net>
2023-12-21 13:09:11 -0600

    Skeleton for CLI

--- build.config.ts
+++ build.config.ts
@@ -1,7 +1,7 @@
 import { defineBuildConfig } from "unbuild";
 
 export default defineBuildConfig({
-    entries: ["src/index", "src/vite-plugin", "src/core/walnut-key"],
+    entries: ["src/index", "src/vite-plugin", "src/core/walnut-key", "src/bin"],
     clean: true,
     declaration: "compatible"
 })
---
+++ src/bin/index.ts
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+// TODO: Actually have options some day. For right now we do nothing with the args
+const [,, ...args] = process.argv;
+
+function runCLI(): void {
+    
+}
